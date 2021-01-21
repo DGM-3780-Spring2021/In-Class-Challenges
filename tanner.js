4219d3337fbd15ffdd27765cdac6ff5a58bc4e8f
Tanner Fronk <tannerjace07@hotmail.com>
2021-01-20 17:08:30 -0700

    Completed 4 tasks

---
+++ tanner.js
@@ -0,0 +1,60 @@
+//**Write a function to replace all the spaces of the string with underscores**
+
+let string = "this is a long string"
+
+function replaceSpace() {
+    string.replace(" ", "_")
+    return string;
+}
+replaceSpace()
+
+
+//**Write a function which can be used to deeply compare 2 nested objects**
+
+const obj1 = {
+    name: 'John',
+    details: {
+        x: 1,
+        y: 2,
+    },
+};
+
+const obj2 = {
+    name: 'John',
+    details: {
+        y: 2,
+        x: 1,
+    },
+};
+
+function compareObjs() {
+    if(JSON.stringify(obj1) === JSON.stringify(obj2)){
+        return true
+    } else {
+        return false
+    }
+}
+
+//**Sort the given array of integers in ascending or descending order**
+
+myArr = ["1","2","3","4","5","6","7","8","9"];
+
+function sortArr(){
+    myArr.sort();
+}
+sortArr()
+
+//**Print numbers from 1 to 10 with delay of 1 second between each value being printed**
+
+function waiting(){
+    console.log("waiting 1 second");
+}
+
+function printNums(){
+    newArr = ["1","2","3","4","5","6","7","8","9","10"]
+    for(let i = 0; i <10; i++){
+        console.log(newArr[i])
+        setTimeout(waiting, 1000)
+    }
+}
+printNums()
